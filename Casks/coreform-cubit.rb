5acba249b38bc05a9401a8fff1f5b2430fd65684
Kevin Tew <kevin.tew@gmail.com>
2021-03-24 11:13:42 -0600

    Initial brew cask test

---
+++ Casks/coreform-cubit.rb
@@ -0,0 +1,11 @@
+cask "coreform-cubit" do
+  version "2021.3"
+  sha256 "8785af79c85868d0ec5154d64febb0af2012afeeabb5a2a3a0d8166862e04804"
+
+  url "https://f002.backblazeb2.com/file/cubit-downloads/Releases/MacOS/Coreform-Cubit-2021.3.0.tar.gz"
+  name "Coreform Cubit"
+  desc ""
+  homepage "https://coreform.com"
+
+  app "Coreform-Cubit-2021.3-Mac64/Coreform-Cubit-2021.3.app"
+end
